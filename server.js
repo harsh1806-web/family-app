20e7fffd50399c8c5b8b3c71a68ae75110179671
harsh1806-web <harshsanghavi07@gmail.com>
2026-04-03 20:22:12 +0530

    fix port for render

--- server.js
+++ server.js
@@ -138,6 +138,6 @@ app.get('/admin/family-members/:id', auth, adminOnly, (req, res) => {
 });
 
 // START SERVER
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
 });
