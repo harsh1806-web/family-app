fcc35f8994deb19793d98f515415768825bf8214
harsh1806-web <harshsanghavi07@gmail.com>
2026-04-07 14:59:09 +0530

    otp

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const axios = require("axios")
+const axios = require("axios");
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 const path = require("path");
 const app = express();
+
+let otpStore = {};
 
 // ✅ Middleware
 app.use(cors());
@@ -96,9 +98,11 @@ app.post("/forgot-password", async (req, res) => {
   const { email } = req.body;
 
   // generate 6 digit OTP
-  const otp = Math.floor(100000 + Math.random() * 900000);
+  const otp = Math.floor(100000 + Math.random() * 900000).toString();
 
   console.log("OTP:", otp);
+
+  otpStore[email] = otp;
 
   try {
     await axios.post(
